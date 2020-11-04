ae36a6579086c529a368df4dd4d2da2884f661c5
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-11-04 02:14:43 -0300

    fix: gateway url

--- packages/constants/src/urls.ts
+++ packages/constants/src/urls.ts
@@ -1,7 +1,7 @@
 export default {
   Site: 'https://kaelbot.xyz',
   Api: 'https://api.kaelbot.xyz',
+  Gateway: 'wss://gateway.kaelbot.xyz',
   Dashboard: 'https://dash.kaelbot.xyz',
   Webhook: 'https://webhook.kaelbot.xyz',
-  Gateway: 'https://gateway.kaelbot.xyz',
 };
