6043ec9db65813bf0f56265e66799433f40b135f
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-11-04 01:35:08 -0300

    chore: remove unnecessary files

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,12 +1,11 @@
 export { default as KaelDatabase } from './KaelDatabase';
-export { default as AdonisDatabase } from './AdonisDatabase';
+
+export { default as UserSchema } from './schemas/User';
+export { default as GuildSchema } from './schemas/Guild';
 
 export { default as BaseConnection } from './connections/BaseConnection';
 export { default as UserConnection } from './connections/UserConnection';
 export { default as GuildConnection } from './connections/GuildConnection';
-
-export { default as UserSchema } from './schemas/User';
-export { default as GuildSchema } from './schemas/Guild';
 
 export { default as Util } from './utils/Util';
 
